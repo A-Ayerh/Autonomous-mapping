6a3e59bcac1874d1a192a7b23aa0e423492ddded
A-Ayerh <shutoyster@gmail.com>
2023-11-07 02:10:33 -0700

    can acces setting fro new options tab

--- BezierCurveApplication.java
+++ BezierCurveApplication.java
@@ -1,4 +1,7 @@
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.SwingUtilities;
 
 public class BezierCurveApplication {
@@ -10,16 +13,30 @@ public class BezierCurveApplication {
     }
 
     private static void createAndShowGui() {
-        JFrame frame = new JFrame("Bezier Curve Drawer");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(new BezierCurveDrawer());
-        frame.setSize(800, 600);
-        frame.setLocationRelativeTo(null);
+    JFrame frame = new JFrame("Bezier Curve Drawer");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.getContentPane().add(new BezierCurveDrawer());
+    frame.setSize(800, 600);
+    frame.setLocationRelativeTo(null);
 
-        // Now create the settings dialog here where you have the frame instance
-        SettingsDialog settingsDialog = new SettingsDialog(frame); // Pass the frame instance
+    // Create a menu bar
+    JMenuBar menuBar = new JMenuBar();
+    frame.setJMenuBar(menuBar);
+
+    // Create a menu
+    JMenu menu = new JMenu("Options");
+    menuBar.add(menu);
+
+    // Create a menu item for settings
+    JMenuItem settingsItem = new JMenuItem("Settings");
+    settingsItem.addActionListener(e -> {
+        SettingsDialog settingsDialog = new SettingsDialog(frame);
         settingsDialog.setVisible(true);
+    });
+    menu.add(settingsItem);
 
-        frame.setVisible(true);
-    }
+    // Now the settings can be accessed via the "Options" menu
+    frame.setVisible(true);
+}
+
 }
